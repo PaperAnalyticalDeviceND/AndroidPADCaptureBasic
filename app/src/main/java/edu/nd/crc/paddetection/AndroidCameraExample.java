b5f531736c374f3803d2532ab261f7cb27e3cfda
James Sweet <james.sweet88@googlemail.com>
2016-06-15 13:39:17 -0400

    Added code to look for contours of image once saved and the start of the white balancing code

--- app/src/main/java/edu/nd/crc/paddetection/AndroidCameraExample.java
+++ app/src/main/java/edu/nd/crc/paddetection/AndroidCameraExample.java
@@ -1,11 +1,17 @@
 package edu.nd.crc.paddetection;
 
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 import android.app.Activity;
 import android.content.Context;
@@ -23,8 +29,12 @@ import android.widget.Toast;
 import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
+import java.util.Vector;
 
 public class AndroidCameraExample extends Activity implements CvCameraViewListener2 {
 	private JavaCamResView mOpenCvCameraView;
@@ -34,6 +44,7 @@ public class AndroidCameraExample extends Activity implements CvCameraViewListen
     }
 
     private Mat mRgba;
+    private double IMAGE_WIDTH = 600;
 
     @Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -59,6 +70,7 @@ public class AndroidCameraExample extends Activity implements CvCameraViewListen
 
                 Mat mTemp = new Mat();
                 Mat result = new Mat(mRgba, new Rect(105, 120, mRgba.width()-172, mRgba.height()-240));
+                Core.flip(result.t(), result, 1);
 
                 boolean Success = true;
 
@@ -78,6 +90,8 @@ public class AndroidCameraExample extends Activity implements CvCameraViewListen
                 }else{
                     Toast.makeText(context, "Save Failed", Toast.LENGTH_SHORT).show();
                 }
+
+                RectifyImage(outputFile.getPath());
             }
         });
     }
@@ -115,4 +129,223 @@ public class AndroidCameraExample extends Activity implements CvCameraViewListen
         mRgba = inputFrame.rgba();
 		return inputFrame.rgba();
 	}
+
+    private class DataPoint implements Comparable<DataPoint>  {
+        public int i;
+        public float Distance, Diameter;
+        public Point Center;
+        public Boolean valid = true;
+
+        public DataPoint(int ii, float id, float idi, Point imc){
+            i = ii;
+            Distance = id;
+            Diameter = idi;
+            Center = imc;
+        }
+
+        public int compareTo(DataPoint other) {
+            return new Float(Distance).compareTo(new Float(other.Distance));
+        }
+    };
+
+    public void RectifyImage(String path){
+        Mat input = Highgui.imread(path);
+        Mat gray = new Mat();
+
+        balance_white(input);
+
+        Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGB2GRAY);
+
+        float ratio = (float)input.size().width / (float)IMAGE_WIDTH;
+
+        Mat work = new Mat();
+        Imgproc.resize(gray, work, new Size(IMAGE_WIDTH, (input.size().height * IMAGE_WIDTH) / input.size().width), 0, 0, Imgproc.INTER_LINEAR );
+        Mat output = work.clone();
+
+
+        Mat work_blur = new Mat();
+        Imgproc.blur(work, work_blur, new Size(2, 2));
+
+        Mat edges = work.clone();
+        Imgproc.Canny(edges, edges, 40, 150, 3, true);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        if( contours.size() > 0 ) {
+            int iBuff[] = new int[ (int) (hierarchy.total() * hierarchy.channels()) ];
+            hierarchy.get(0, 0, iBuff);
+
+            Vector<Integer> Markers = new Vector<>();
+            for (int i = 0; i < contours.size(); i++) {
+                int k = i;
+                int c = 0;
+
+                while (iBuff[k*4+2] != -1) {
+                    k = iBuff[k*4+2];
+                    c = c + 1;
+                }
+
+                if (iBuff[k*4+2] != -1) {
+                    c = c + 1;
+                }
+
+                if (c >= 3) {
+                    Markers.add(i);
+                }
+            }
+
+            List<DataPoint> order = new Vector<>();
+            for( int i=0; i < Markers.size(); i++){
+                Imgproc.drawContours(output, contours, Markers.get(i), new Scalar(255, 200, 0), 2, 8, hierarchy, 0, new Point(0, 0));
+
+                Moments mum = Imgproc.moments(contours.get(Markers.get(i)), false);
+                Point mc = new Point( mum.get_m10()/mum.get_m00() , mum.get_m01()/mum.get_m00() );
+
+                //calculate distance to nearest edge
+                float dist = Math.min(Math.min(Math.min((float)mc.x, (float)(IMAGE_WIDTH - mc.x)), (float)mc.y), (float)(input.size().height - mc.y));
+
+                Rect box = Imgproc.boundingRect(contours.get(Markers.get(i)));
+
+                float dia = Math.max((float)box.width, (float)box.height) * 0.5f;
+
+                //only add it if sensible
+                if(dia < 30 && dia > 15){
+                    order.add(new DataPoint(i, dist, dia, mc));
+                }
+            }
+
+            for( int i=0; i < order.size(); i++){
+                if(order.get(i).valid){
+                    for( int j=i+1; j < order.size(); j++){
+                        if(order.get(j).valid){
+                            double ix = order.get(i).Center.x;
+                            double iy = order.get(i).Center.y;
+                            double jx = order.get(j).Center.x;
+                            double jy = order.get(j).Center.y;
+
+                            if(Math.abs(ix - jx) < 5 && Math.abs(iy - jy) < 5){
+                                if(order.get(i).Diameter < order.get(j).Diameter){
+                                    order.get(i).valid = false;
+                                    break;
+                                }else{
+                                    order.get(j).valid = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Collections.sort(order);
+
+            Point com = new Point(0.0, 0.0);
+            float pcountf = 0.0f;
+
+            for( int i=0; i<order.size(); i++){
+                if(order.get(i).valid){
+                    com.x += order.get(i).Center.x;
+                    com.y += order.get(i).Center.y;
+                    pcountf += 1.0;
+                }
+            }
+
+            com.x /= pcountf;
+            com.y /= pcountf;
+            Core.circle(output, com, 10, new Scalar(0, 255, 255), 1, 8, 0);
+
+            //count points
+            int pcount = 0;
+
+            //loop
+            for( int j=0; j<order.size(); j++){
+                if(order.get(j).valid){
+                    float dia = 0.0f;
+                    Point mcd = order.get(j).Center;
+
+                    //if top LHS then QR code marker
+                    if(mcd.x < (com.x + 30) && mcd.y < com.y){
+                        dia = 27;
+                    }else{
+                        dia = 20;
+                    }
+
+                    Core.circle(output, mcd, (int)dia, new Scalar(0, 0, 255), 1, 8, 0);
+
+                    Log.i("Contours", String.format("Point: %d, %d, %d", (int)(mcd.y * ratio + 0.5), (int)(mcd.x * ratio  + 0.5), (int)(dia * ratio + 0.5)));
+                    if(pcount++ >= 5) break;
+                }
+            }
+
+            File SDlocation = Environment.getExternalStorageDirectory();
+            File padImageDirectory = new File(SDlocation + "/images/");
+            padImageDirectory.mkdirs();
+
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+            Date today = Calendar.getInstance().getTime();
+
+            File outputFile = new File(padImageDirectory, df.format(today) + "-rectify.jpeg");
+            if(Highgui.imwrite(outputFile.getPath(), output) ) {
+                Intent intentA = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                intentA.setData(Uri.fromFile(outputFile));
+                sendBroadcast(intentA);
+            }
+        }
+    }
+
+    void balance_white(Mat mat) {
+        double discard_ratio = 0.05;
+        int[][] hists = new int[3][256];
+        for( int i = 0; i < 3; i++ ){
+            for( int j = 0; j < 256; j++) {
+                hists[i][j] = 0;
+            }
+        }
+
+        for (int y = 0; y < mat.rows(); ++y) {
+            byte buff[] = new byte[mat.cols() * mat.channels()];
+            mat.get(y, 0, buff);
+            for (int x = 0; x < mat.cols(); ++x) {
+                for (int j = 0; j < 3; ++j) {
+                    hists[j][buff[x * 3 + j]] += 1;
+                }
+            }
+        }
+
+        // cumulative hist
+        int total = mat.cols()*mat.rows();
+        int[] vmin = new int[3];
+        int[] vmax = new int[3];
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 255; ++j) {
+                hists[i][j + 1] += hists[i][j];
+            }
+            vmin[i] = 0;
+            vmax[i] = 255;
+            while (vmin[i] < 255 && hists[i][vmin[i]] < discard_ratio * total) {
+                vmin[i] += 1;
+            }
+            while (vmin[i] > 0 && hists[i][vmax[i]] > (1 - discard_ratio) * total) {
+                vmax[i] -= 1;
+            }
+            if (vmax[i] < 255 - 1) vmax[i] += 1;
+        }
+
+
+        for (int y = 0; y < mat.rows(); ++y) {
+            for (int x = 0; x < mat.cols(); ++x) {
+                byte buff[] = new byte[mat.cols() * mat.channels()];
+                mat.get(y, 0, buff);
+
+                for (int j = 0; j < 3; ++j) {
+                    int val = (int)mat.get(y,x)[j];
+                    if (val < vmin[j]) val = vmin[j];
+                    if (val > vmax[j]) val = vmax[j];
+                    buff[x * 3 + j] = (byte)((val - vmin[j]) * 255.0 / (vmax[j] - vmin[j]));
+                }
+                mat.put(y, 0, buff);
+            }
+        }
+    }
 }

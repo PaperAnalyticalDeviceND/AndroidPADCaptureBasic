380c72b0af9e95737a732a6421c533c3a20098b2
Chris Sweet <csweet1@nd.edu>
2017-05-11 09:40:32 -0400

    Put James interpretation of contour detection back in with scaling of points back to image.

--- app/src/main/java/edu/nd/crc/paddetection/AndroidCameraExample.java
+++ app/src/main/java/edu/nd/crc/paddetection/AndroidCameraExample.java
@@ -1,14 +1,18 @@
 package edu.nd.crc.paddetection;
 
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.core.MatOfPoint;
 import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -57,6 +61,8 @@ public class AndroidCameraExample extends Activity implements CvCameraViewListen
     private static String[] IMAGENET_CLASSES;
     private CaffeMobile caffeMobile;
     private ProgressDialog dialog;
+    private Mat mRgbaModified;
+    private static int IMAGE_WIDTH = 600;
 
     @Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -149,9 +155,163 @@ public class AndroidCameraExample extends Activity implements CvCameraViewListen
 
 	}
 
-	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+    private class DataPoint implements Comparable<DataPoint>  {
+        public int i;
+        public float Distance, Diameter;
+        public Point Center;
+        public Boolean valid = true;
+
+        public DataPoint(int ii, float id, float idi, Point imc){
+            i = ii;
+            Distance = id;
+            Diameter = idi;
+            Center = imc;
+        }
+
+        public int compareTo(DataPoint other) {
+            return new Float(Distance).compareTo(new Float(other.Distance));
+        }
+    };
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
-		return inputFrame.rgba();
+        mRgbaModified = inputFrame.rgba();
+
+        float ratio = (float)mRgba.size().width / (float)IMAGE_WIDTH;
+
+        Mat work = new Mat();
+        Imgproc.resize(inputFrame.gray(), work, new Size(IMAGE_WIDTH, (mRgba.size().height * IMAGE_WIDTH) / mRgba.size().width), 0, 0, Imgproc.INTER_LINEAR );
+
+        //inputFrame.gray().copyTo(work);
+
+        Mat work_blur = new Mat();
+        Imgproc.blur(work, work_blur, new Size(2, 2));
+
+
+        Mat edges = work.clone();
+        Imgproc.Canny(edges, edges, 40 , 150, 3, true);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        if( contours.size() > 0 ) {
+            int iBuff[] = new int[ (int) (hierarchy.total() * hierarchy.channels()) ];
+            hierarchy.get(0, 0, iBuff);
+
+            Vector<Integer> Markers = new Vector<>();
+            for (int i = 0; i < contours.size(); i++) {
+                int k = i;
+                int c = 0;
+
+                while (iBuff[k*4+2] != -1) {
+                    k = iBuff[k*4+2];
+                    c = c + 1;
+                }
+
+                if (iBuff[k*4+2] != -1) {
+                    c = c + 1;
+                }
+
+                if (c >= 5) {
+                    Markers.add(i);
+                }
+            }
+
+            List<DataPoint> order = new Vector<>();
+            for( int i=0; i < Markers.size(); i++){
+                Imgproc.drawContours(mRgbaModified, contours, Markers.get(i), new Scalar(255, 200, 0), 2, 8, hierarchy, 0, new Point(0, 0));
+
+                Moments mum = Imgproc.moments(contours.get(Markers.get(i)), false);
+                Point mc = new Point( mum.get_m10()/mum.get_m00() , mum.get_m01()/mum.get_m00() );
+
+                //calculate distance to nearest edge
+                float dist = Math.min(Math.min(Math.min((float)mc.x, (float)(IMAGE_WIDTH - mc.x)), (float)mc.y), (float)(inputFrame.rgba().size().height - mc.y));
+
+                Rect box = Imgproc.boundingRect(contours.get(Markers.get(i)));
+
+                float dia = Math.max((float)box.width, (float)box.height) * 0.5f;
+
+                //only add it if sensible
+                //if(dia < 30 && dia > 15){
+                    order.add(new DataPoint(i, dist, dia, mc));
+                //}
+            }
+
+            for( int i=0; i < order.size(); i++){
+                if(order.get(i).valid){
+                    for( int j=i+1; j < order.size(); j++){
+                        if(order.get(j).valid){
+                            double ix = order.get(i).Center.x;
+                            double iy = order.get(i).Center.y;
+                            double jx = order.get(j).Center.x;
+                            double jy = order.get(j).Center.y;
+
+                            if(Math.abs(ix - jx) < 5 && Math.abs(iy - jy) < 5){
+                                if(order.get(i).Diameter < order.get(j).Diameter){
+                                    order.get(i).valid = false;
+                                    break;
+                                }else{
+                                    order.get(j).valid = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Collections.sort(order);
+
+            //get center of mass
+            Point com = new Point(0.0, 0.0);
+            float pcountf = 0.0f;
+
+            for( int i=0; i<order.size(); i++){
+                if(order.get(i).valid){
+                    com.x += order.get(i).Center.x;
+                    com.y += order.get(i).Center.y;
+                    pcountf += 1.0;
+                }
+            }
+
+            com.x /= pcountf;
+            com.y /= pcountf;
+
+            //scale back to image size
+            Point comDisplay = new Point(com.x * ratio, com.y * ratio);
+
+            Core.circle(mRgbaModified, comDisplay, 10, new Scalar(0, 255, 255), 1, 8, 0);
+
+            //count points
+            int pcount = 0;
+
+            //loop
+            for( int j=0; j<order.size(); j++){
+                if(order.get(j).valid){
+                    float dia = 0.0f;
+                    Point mcd = order.get(j).Center;
+
+                    //if top LHS then QR code marker
+                    if(mcd.x < (com.x + 30) && mcd.y < com.y){
+                        dia = 27;
+                    }else{
+                        dia = 20;
+                    }
+
+                    Log.i("Contours", String.format("Point: %d, %d, %d", (int)(mcd.y * ratio + 0.5), (int)(mcd.x * ratio  + 0.5), (int)(dia * ratio + 0.5)));
+
+                    //scale back to image size
+                    mcd.x *= ratio;
+                    mcd.y *= ratio;
+                    Core.circle(mRgbaModified, mcd, (int)dia, new Scalar(255, 0, 0), 1, 8, 0);
+
+                    if(pcount++ >= 5) break;
+                }
+            }
+
+        }
+
+        return mRgbaModified;
 	}
 
     public class PredictionGuess implements Comparable<PredictionGuess> {
